257bf1d09475b354ee3f59c96136df65d5b49db5
jaimewukb <jaime.wu@kiwibank.co.nz>
2024-12-11 17:04:24 +1300

    theme toggle change icon

--- bpdiary-web-next15/src/app/_components/navigation/themeToggle.tsx
+++ bpdiary-web-next15/src/app/_components/navigation/themeToggle.tsx
@@ -1,14 +1,20 @@
 "use client";
-import { Sun } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { SidebarMenuButton } from "../shadcn/sidebar";
 
 export default function ThemeToggle() {
   const toggleTheme = () => {
+    const theme = localStorage.getItem("theme");
+    localStorage.setItem("theme", theme === "light" || theme === null ? "dark" : "light");
     document.documentElement.classList.toggle("dark");
   };
   return (
-    <SidebarMenuButton onClick={toggleTheme} className="h-12 pl-4 group-data-[collapsible=icon]:!size-12 group-data-[collapsible=icon]:!pl-4">
-      <Sun />
+    <SidebarMenuButton
+      onClick={toggleTheme}
+      className="h-12 pl-4 group-data-[collapsible=icon]:!size-12 group-data-[collapsible=icon]:!pl-4"
+    >
+      <Sun className="dark:hidden"/>
+      <Moon className="hidden dark:flex"/>
       <span>Appearance</span>
     </SidebarMenuButton>
   );
