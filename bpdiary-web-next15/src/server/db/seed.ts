3e520d0dc24a9088c436fb18bd9254c185317d8e
pengi <jaime.wu011@gmail.com>
2025-02-02 20:03:44 +1300

    edit seed db

--- bpdiary-web-next15/src/server/db/seed.ts
+++ bpdiary-web-next15/src/server/db/seed.ts
@@ -11,14 +11,14 @@ const conn = postgres(env.DATABASE_URL);
 const db = drizzle(conn);
 
 await reset(db, { bloodPressure });
-await seed(db, { bloodPressure }, { count: 20 }).refine((funcs) => ({
+await seed(db, { bloodPressure }, { count: 100 }).refine((funcs) => ({
     bloodPressure: {
         columns: {
             loggedByUserId: funcs.valuesFromArray({ values: ["a989cf00-ff8e-4824-b71a-5ef3a7befdf0"] }),
             systolic: funcs.int({minValue: 100, maxValue: 160}),
             diastolic: funcs.int({minValue: 70, maxValue: 90}),
             pulse: funcs.int({minValue: 60, maxValue: 100}),
-            measuredAt: funcs.date({maxDate: new Date(), minDate: startOfWeek(new Date())})
+            measuredAt: funcs.date({maxDate: new Date(), minDate: startOfYear(new Date())})
         }
     }
 }));
