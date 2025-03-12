d90e7576d34308cacf8001e19f4ec1a8cb4599f2
pengi <jaime.wu011@gmail.com>
2025-03-12 17:06:07 +1300

    fix home header issues

--- bpdiary-web-next15/src/app/_components/loading-states/loading-header.tsx
+++ bpdiary-web-next15/src/app/_components/loading-states/loading-header.tsx
@@ -16,13 +16,13 @@ export default async function LoadingHeader() {
             buttonVariants({ size: "circular", variant: "muted" }),
           )}
         />
-        <Skeleton className="mr-auto h-10 w-[30rem] rounded-lg align-middle" />
+        <Skeleton className="tablet:block mr-auto hidden h-10 w-[30rem] rounded-lg align-middle" />
         <LogBpFormTrigger variant="muted" size="circular" />
         <Button variant="muted" size="circular">
           <Bell className="size-[1.5rem]" />
         </Button>
       </div>
-      <Skeleton className="tablet:hidden mr-auto pt-4 align-middle text-3xl text-[1.5rem] leading-none font-semibold tracking-tight" />
+      <Skeleton className="tablet:hidden mr-auto h-10 w-[30rem] rounded-lg align-middle" />
       <Skeleton className="mr-auto h-4 w-[30rem] rounded-lg align-middle" />
     </BaseHeader>
   );
